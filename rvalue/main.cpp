37527cb68c534f30e1741282bf0ddef304afe0e3
gmoccand <gael.moccand@gmail.com>
2020-11-04 10:49:53 +0100

    refator code a bit to be more clean

--- rvalue/main.cpp
+++ rvalue/main.cpp
@@ -22,11 +22,11 @@ Note: this code is not optimal yet. Copy-and-swap idiom should be used to avoid 
 
 class Vector{
  private:
-    double* elem;
-    size_t sz;
+    double* m_elem;
+    size_t m_sz;
  public:
     explicit Vector(size_t s);
-    ~Vector(){delete[] elem;}
+    ~Vector(){delete[] m_elem;}
 
     Vector(const Vector& vec);
     Vector& operator=(const Vector& vec);
@@ -43,82 +43,76 @@ class Vector{
 Vector::Vector(size_t s)
 {
     std::cout << "ctor" << std::endl;
-    if(s < 0)
+    if (s < 0)
     {
         throw std::length_error{"Vector ctor: negative size"};
     }
-    elem = new double[s];
-    sz=s;
+    m_elem = new double[s];
+    m_sz=s;
 }
 
 Vector::Vector(const Vector& other)
-:elem{new double[other.sz]},sz{other.sz}            // input const -> left untouched; create a new array with the same size
+:m_elem{new double[other.sz]},m_sz{other.sz}            // input const -> left untouched; create a new array with the same size
 {
     std::cout << "copy ctor" << std::endl;
-    std::copy(other.elem, other.elem + sz, elem);   // deep copy is required
+    std::copy(other.m_elem, other.m_elem + m_sz, m_elem);   // deep copy is required
 }
 
 Vector& Vector::operator=(const Vector& other) // input const -> left untouched
 {
     std::cout << "copy assignement" << std::endl;
     if(this == &other) return *this;    // check for self assignment
-    delete[] elem;
-    elem = new double[other.sz];
-    std::copy(other.elem, other.elem + other.sz, elem);
-    sz = other.sz;
+    delete[] m_elem;
+    m_elem = new double[other.m_sz];
+    std::copy(other.m_elem, other.m_elem + other.m_sz, m_elem);
+    m_sz = other.sz;
     return *this;                       // by convention a reference to this class is returned
 }
 
 Vector::Vector(Vector&& other)
-:elem{other.elem},sz{other.sz}          // steal the data first for the rvalue reference
+:m_elem{other.m_elem},m_sz{other.sz}          // steal the data first for the rvalue reference
 {
                                         // no deep copy involved here just moving ressources
     std::cout << "mv ctor" << std::endl;
-    other.elem = nullptr;               // important to set rvalue ref data to valid state
-    other.sz = 0;                       // to preven it being accidentally delted when the temporary object dies
+    other.m_elem = nullptr;               // important to set rvalue ref data to valid state
+    other.m_sz = 0;                       // to preven it being accidentally delted when the temporary object dies
 }
 
 Vector& Vector::operator=(Vector&& other)
 {
     std::cout << "mv assignement" << std::endl;
 
-    if(this == &other) return *this;    // check for self assignment
-    delete[] elem;                      // clean of actual ressource
-    elem = other.elem;
-    sz = other.sz;
+    if (this == &other) return *this;    // check for self assignment
+    delete[] m_elem;                      // clean of actual ressource
+    m_elem = other.m_elem;
+    m_sz = other.sz;
 
-    other.elem = nullptr;               // put temp. object in valid state
-    other.sz = 0;
+    other.m_elem = nullptr;               // put temp. object in valid state
+    other.m_sz = 0;
     return *this;                       // by convention a reference to this class is returned
 }
 
 const double& Vector::operator[](int i) const 
 {
-    if(i < 0 || size() <= i)
+    if (i < 0 || size() <= i)
     {
         throw std::out_of_range{"Vector ctor: negative size"};
     }
-    return elem[i];
+    return m_elem[i];
 }
 
 double& Vector::operator[](int i) 
 {
-    if(i < 0 || size() <= i)
+    if (i < 0 || size() <= i)
     {
         throw std::out_of_range{"Vector ctor: negative size"};
     }
-    return elem[i];
+    return m_elem[i];
 }
 
 int Vector::size() const
 {
-    return sz;
-}
-
-Vector makeVector()
-{
-    Vector vec{2};
-    return (vec); // compiler must elide copy or std:move(vec)
+    return m_sz;
 }
 
 Vector factory(size_t s)
@@ -126,14 +120,22 @@ Vector factory(size_t s)
     return Vector(s);
 }
 
+
+Vector makeVector()
+{
+    Vector vec{2};
+    // return (vec); // compiler must elide copy OR std:move(vec)
+    return std::move(vec); // try to use std::move and observer the impact on RVO
+}
+
 int main()
 {
 
     std::cout << "Vector vec1(3); \n";
     Vector vec1(3);         // ctor
-    vec1[0]=1;
-    vec1[1]=2;
-    vec1[2]=3;
+    vec1[0] = 1;
+    vec1[1] = 2;
+    vec1[2] = 3;
     std::cout << std::endl;
 
     std::cout << "Vector vec2 = vec1; \n";
@@ -156,15 +158,15 @@ int main()
     std::cout << std::endl;
 
     std::cout << "Vector vec6 = factory(3); \n";
-    Vector vec6 = factory(3);
+    Vector vec6 = factory(3); // RVO should occur
     std::cout << std::endl;
 
     std::cout << "vec6 = factory(4); \n";
-    vec6 = factory(4);
+    vec6 = factory(4); // RVO should occur
     std::cout << std::endl;
 
     std::cout << "Vector vec7 = makeVector(); \n";
-    Vector vec7 = makeVector();
+    Vector vec7 = makeVector(); // RVO should occur
     std::cout << std::endl;
 }
 
