da4cbff9634db3719bebdbf31c81fb8a6f5f4f7e
gmoccand <gael.moccand@gmail.com>
2021-01-27 15:30:42 +0100

    add erase & erase_if c++ 20

--- containers/remove_elements.cpp
+++ containers/remove_elements.cpp
@@ -2,14 +2,35 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <experimental/vector>
 
+
+template <typename T>
+void erase(std::vector<T>& vec, const T& elem)
+{
+    vec.erase(std::remove(begin(vec), end(vec), elem), end(vec));
+}
+
+void erase(std::string& str, const char elem)
+{
+    str.erase(std::remove(begin(str), end(str), elem), end(str));
+}
+
+void disp(const std::vector<int>& vec)
+{
+    for(const auto& el : vec )
+    {
+        std::cout << el;
+    }
+    std::cout << "\n";
+}
 
 int main() {
     using namespace std;
 
 
-    vector<int> myvec{1,2,3,4,5,6,7};
-    string mystr{"Hi Folks !"};
+    vector<int> myvec{1,2,3,4,5,6,7,8,8,9,9,-1,-2};
+    string mystr{"Hi Folks !  !"};
 
     cout << myvec[1] << "\n";
     cout << mystr << "\n";
@@ -19,30 +40,45 @@ int main() {
     mystr.erase(mystr.begin() + 1);
     cout << myvec[1] << "\n";
     cout << mystr << "\n";
-    // for a subrange
+    // for a subrange (important for next point)
     myvec.erase(myvec.begin(), myvec.begin()+2); // erase the first 2 el.
     mystr.erase(0, 2);
     cout << myvec[1] << "\n";
     cout << mystr << "\n";    
 
     // 2. remove element that match a specific value
-    // using erase remove C++ idiom
-    myvec.erase(std::remove(begin(myvec), end(myvec), 6), end(myvec));
-    for(const auto& el : myvec )
-    {
-        cout << el;
-    }
-    cout << "\n";
+    // using erase_remove C++ idiom
+    auto iter = std::remove(begin(myvec), end(myvec), 6);
+    myvec.erase(iter, end(myvec));
+    disp(myvec);
 
     mystr.erase(std::remove(begin(mystr), end(mystr), '!'), end(mystr));
     cout << mystr << "\n";
 
+    erase(myvec, 7);
+    disp(myvec);
 
+    erase(mystr,'s');               
+    cout << mystr << "\n";
 
+    // 3. remove element with predicate using 
+    // The most practical use for remove and remove_if is to remove elements from a range.
 
+    auto negativeNumber = [](auto const & elem) { return elem < 0;};
+    auto it = std::remove_if(myvec.begin(), myvec.end(), negativeNumber);
+    myvec.erase(it, myvec.end());
+    disp(myvec);
 
+    // 4. remove adjacent duplicates using unique
 
+    myvec.erase(std::unique(myvec.begin(),myvec.end()),myvec.end());
+    disp(myvec);
+
+    // 5. erase and erase_if finally available in C++20
+    experimental::erase(myvec,8);
+    disp(myvec);
+
+    auto divisibleBy3 = [](auto const& elem) { return (elem % 3) == 0; };
+    experimental::erase_if(myvec, divisibleBy3);
+    disp(myvec);
 }
-
-
-
