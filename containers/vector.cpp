86836d550ddf823284a7c61a2232ad676703c6b0
gmoccand <gael.moccand@gmail.com>
2021-03-18 17:18:33 +0100

    add at() for vector and add some comments

--- containers/vector.cpp
+++ containers/vector.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+    #include <vector>
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -26,19 +26,37 @@ int main() {
 
     using namespace std;
 
-    vector<int> v1 = {1,10,20,5,40,50}; 
+    vector<int> v1 = {1, 10, 20, 5, 40, 50}; 
 
-    // 1. using [] to add and modify element
+    // 1. using [] to acess and modify element
     v1[0] = 11;
     v1[1] += 2; // modify directly
     v1[2]++;
+    v1[6] = 0; // boom no out of range check !! Undefined behavior
 
-    // 2. using insert to add element but does not overwrite
+    print(v1);
+
+    // 2. using at is much better. Throw out of range exception
+
+    v1.at(0) = 10;
+    try
+    {
+        v1.at(7) = 10;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    
+
+    // 3. using insert to add element but does not overwrite
+    // This is an excpensive operation if not insert at the end()
+    // it cause container to relocate all the elements that were after position to their new positions.
     auto it = v1.insert(v1.end(), 3);
     print(v1);
 
-    // 3. using emplace_back or push_back to insert
-    v1.emplace_back(60); // more performant construct the object directly in container
+    // 4. using emplace_back or push_back to insert
+    v1.emplace_back(60); // more performant construct the object  directly (in place) in container
     v1.push_back(70); // safer according to abseil because more readable
     print(v1);
 
@@ -46,7 +64,7 @@ int main() {
     cout << v1.back() << "\n";
     v1.pop_back();
 
-    // 4. some different ways to copy vectors
+    // 5. some different ways to copy vectors
     auto v2{v1};
     copy(v1.begin(), v1.end(), back_inserter(v2));
     
@@ -68,7 +86,7 @@ int main() {
     print(v1);
 
     // vector of vector
-    vector<vector<int>> v3 = {{1,2,3},{4,5,6}};
+    vector<vector<int>> v3 = {{1, 2, 3}, {4 ,5 ,6}};
     cout << v3[1][1]; // row colum access 2d vec
 
 }
