67b3a6a08d355a80b135d293e4c890816ba1d33c
gmoccand <gael.moccand@gmail.com>
2021-03-31 10:12:22 +0200

    modify remove for maps and add more example with using key and pos

---
+++ containers/rm_maps.cpp
@@ -0,0 +1,56 @@
+#include <map>
+#include <algorithm>
+#include <iostream>
+#include <experimental/map>
+
+template <typename C>
+void disp(const C& cont) {
+
+    for (const auto& elem : cont){
+        std::cout << elem.first << " : ";
+        std::cout << elem.second << "\n";
+    }
+    std::cout << "\n";
+}
+
+int main() {
+
+    using namespace std;
+
+    map<int,int> mymap {{1, 10}, {2, 20}, {3, 30}, {4, 40}, {5, 50}, {6, 60}};
+    cout << "mymap" << "\n";
+    disp(mymap);
+
+    // 1. remove element using a key is a piece ok cake !
+    mymap.erase(5);
+    cout << "remove key 5 in mymap \n";
+    disp(mymap);
+
+    // 2.remove element using iterator
+    auto iter = mymap.end();
+    mymap.erase(--iter);
+    cout << "remove last element in my map \n";
+    disp(mymap);
+
+    
+    auto predEvenKey = [](auto const& elem){ auto const [key, val] = elem; return key % 2 == 0;};
+
+    // 3. trick to remove evenKey using loop < C++20
+    cout << "for loop \n";
+    for(auto it = mymap.begin(); it != mymap.end(); /*not increament here*/) {
+        if(predEvenKey(*it)) {
+            it = mymap.erase(it); // erase returns the iter. following the removed elements
+        }
+        else {
+            ++it;
+        }
+
+    }
+    disp(mymap);
+
+    // 4. remove evenKey using erase_if C++20
+    cout << "erase_if(mymap,evenKey) \n";
+    std::experimental::erase_if(mymap, predEvenKey);
+    disp(mymap);
+    
+}
