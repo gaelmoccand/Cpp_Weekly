721fd10cec252234055bcf53f7f12294d3700f34
gmoccand <gael.moccand@gmail.com>
2021-04-26 15:39:14 +0200

    modify comments to  be a bit cleaner

--- containers/add_maps.cpp
+++ containers/add_maps.cpp
@@ -19,32 +19,32 @@ void disp(const M& container){
 int main() {
 
     using namespace std;
-    map<string,int> phone_book { {"John", 124}, {"Mary", 345}};
+    map<string,int> phone_book { {"John", 124}, {"Mary", 345}, {"Marc", 345}, };
 
-
+    // 1. Using []
     phone_book["Leon"] = 4; // no way to find out if insertion took place
     phone_book["Leon"] += 2; // modify the entry
-
-    phone_book.insert({"Marc", 7876}); // do not overwrite value if key present
-
     disp(phone_book);
 
+    // 1. Using Insert
     // [] and isnert differ when element with givern key exist
     cout << "[\"Leon\"] = 5; insert(std::pair{\"Marc\", 6}); \n"; 
     phone_book["Leon"] = 5; // overwrite
-    phone_book.insert(std::pair{"Marc", 6}); // do not 
+    phone_book.insert(std::pair{"Marc", 6}); // do not overwrite
 
     // C++ 17 style insert with iterator
-    auto [itins ,inserted] = phone_book.insert({"Marc", 7}); // do not overwrite
-    assert(inserted == false); 
+    auto [iter , inserted] = phone_book.insert({"Marc", 7}); // do not overwrite value if key present
+    assert(inserted == false);
     disp(phone_book);
 
+    // 2. Using emplace
     // emplace enables in place construction , we can ommit {}
     // drawback create object even if not inserted + steals argument anyway (unique ptr !!!)
-    phone_book.emplace("Gianna", 799); // do not 
+    phone_book.emplace("Gianna", 799); // do not overwrite
     cout << "emplace(\"Gianna\", 799) : \n"; 
     disp(phone_book);
 
+    // 3. Using try_emplace
     // doesn't generate a redundant object & does not steals argument when insertation failed
     // try_emplace() makes sure that the argument remains untouched in case it wasn’t inserted (unique_ptr())
     phone_book.try_emplace("Ben", 47);
@@ -52,8 +52,9 @@ int main() {
     cout << "try_emplace(\"Ben\", 47) : \n"; 
     disp(phone_book);
 
+    // 4. Using insert_or_assign
     // like [] it modifies values if supplied with a key that is already present in the map
-    // It doesn’t require default constructibility of the value_type
+    // It does not require default constructibility of the value_type
     // returns a pair<iterator, bool> 
     auto [it, insert] = phone_book.insert_or_assign("Ben", 4864654); // overwrite value
     assert(insert == false);
@@ -62,6 +63,4 @@ int main() {
     disp(phone_book);
 
     phone_book.clear();
-
-
 }
