07acf6d8829ba662ae29f65eaaab04112f154f80
gmoccand <gael.moccand@gmail.com>
2021-05-04 16:12:28 +0200

    add optional return from function

--- optional/optional.cpp
+++ optional/optional.cpp
@@ -4,6 +4,8 @@
 #include <complex>
 #include <utility>
 #include <algorithm>
+#include <ctype.h>
+#include <string>
 
 std::optional<std::vector<int>::const_iterator> findElem(const std::vector<int> vec, int target) {
     // copy elision is mandatory in C++17 it's more optimal to use - not named optional - if possible -> not temp. created
@@ -14,6 +16,16 @@ std::optional<std::vector<int>::const_iterator> findElem(const std::vector<int> 
     return std::nullopt; 
 }
 
+std::optional<int> newFct(const std::string& str_dec) {
+    if (isdigit(str_dec[0])) {
+        return std::stoi (str_dec, nullptr, 10);
+    }
+    return std::nullopt;
+}
+
+int legacyFct(const std::string& str_dec) {
+    return newFct(str_dec).value_or(-1);
+}
 
 int main() {
 
@@ -43,14 +55,27 @@ int main() {
     // 2. return optional from a function
     std::vector myVec {1, 2, 3, 4};
     if (auto pos = std::find(myVec.begin(), myVec.end(), 0); pos != myVec.end()) {
-        std::cout << "found elem 0 !";
+        std::cout << "found elem 0 !\n";
     }
 
     // use optional respect the level of abstraction compare to the if()
     auto posElem = findElem(myVec, 2);
     if (posElem) {
-        std::cout << "found elem 2 !";
+        std::cout << "found elem 2 !\n";
     }
+
+    //using optional to refactor legacy code without breakin the API
+    auto intRes = legacyFct({"1234"}); // by convention -1 means no value
+    if (intRes != -1) {
+        std::cout << "converted :" << intRes <<"\n";
+    }
+
+    auto optRes = newFct({"abc"}); // now optional is used. Interface is cleaner
+    if (optRes) {
+        std::cout << "converted :" << optRes.value() <<"\n";
+    }
+
+
 
 
 
