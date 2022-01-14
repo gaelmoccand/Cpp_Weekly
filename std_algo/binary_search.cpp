3003dd86fe6e51532c4e645085f35cf8792ed5cf
Gael Moccand <gael.moccand@ch.abb.com>
2022-01-14 18:59:47 +0100

    add quick sort algo

--- std_algo/binary_search.cpp
+++ std_algo/binary_search.cpp
@@ -8,37 +8,44 @@
 // 2. Binary search O(log(n))
 
 template<typename T, typename K>
-std::optional<int> searchElem(const T& seq, const K elem){
+std::optional<int> searchElem(const T& seq, const K elem) {
     auto first = seq.begin();
     auto last = seq.end();
-    if (elem < *first)
-        return std::nullopt;
-    if (elem > *std::prev(last)) 
-        return std::nullopt;
+    if (elem < *first || elem > *std::prev(last)) return std::nullopt;
 
-    auto N = std::distance(first, last);
     while (first < last) {
-        auto const pivot = std::next(first, N/2);
-        if (*pivot == elem) {
+        auto const N = std::distance(first, last);
+        auto const pivot = std::next(first, N / 2);
+        if (elem == *pivot) {
             auto found = std::distance(seq.begin(), pivot);
             return std::make_optional(found);
         }
-        else if ( elem < *pivot) {
+        else if (elem < *pivot) {
             last = pivot;
         }
         else {
             first = pivot;
         }
-        N = std::distance(first, last);
     }
     return std::nullopt;
 }
 
+template<typename FwdIt, typename Compare = std::less<>>
+void quickSort(FwdIt first, FwdIt last, Compare cmp = Compare{}) {
+    auto const N = std::distance(first, last);
+    if (N <= 1) return; 
+    auto const pivot = std::next(first, N / 2);
+    std::nth_element(first, pivot, last, cmp);
+    quickSort(first, pivot, cmp); 
+    quickSort(pivot, last, cmp); 
+}
+
 int main() {
 
-    auto v4 = std::vector{0,1,2,3,4,5,6,7,8}; // sorted vector
-    
-    if (auto position = searchElem(v4, 9); position) {
+    auto v4 = std::vector{7,0,1,3,4,5,6,2,8};
+    quickSort(v4.begin(), v4.end());
+
+    if (auto position = searchElem(v4, 8); position) {
         std::cout << "found at pos :" << *position;
     }
 }
